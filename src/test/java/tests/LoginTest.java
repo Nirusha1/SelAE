d8a6e2ce0d1cba411e591b06edbfb867ebf6905d
Nirusha1 <bhintunasayami123@gmail.com>
2025-06-09 10:14:20 -0400

    Add extent report listener on LoginTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,13 +1,18 @@
 package tests;
 
+import listeners.ExtentTestNGListener;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import utils.BaseTest;
 
+@Listeners(ExtentTestNGListener.class)
 public class LoginTest extends BaseTest {
     @Test
+
     public void testValidLogin() {
+        ExtentTestNGListener.test.get().info("Navigating to login page");
         driver.get("https://automationexercise.com/");
 
         LoginPage loginPage = new LoginPage(driver);
