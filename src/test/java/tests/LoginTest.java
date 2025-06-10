717112ba6c5d22110a02bbd290471b0ad851dc30
Nirusha1 <bhintunasayami123@gmail.com>
2025-06-10 14:00:50 -0400

    Update report listener for login

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -13,13 +13,17 @@ public class LoginTest extends BaseTest {
 
     public void testValidLogin() {
         ExtentTestNGListener.test.get().info("Navigating to login page");
-        driver.get("https://automationexercise.com/");
+        String currentURL = "https://automationexercise.com/";
+        driver.get(currentURL);
+        ExtentTestNGListener.test.get().info("Navigated to: " + currentURL);
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.goToLoginPage();
 
         loginPage.login("bhintunasayami123@gmail.com", "Selenium123");
+        ExtentTestNGListener.test.get().info("Entered email"+"bhintunasayami123@gmail.com");
 
         Assert.assertTrue(loginPage.isLoginSuccessful(), "Login was not successful.");
+        ExtentTestNGListener.test.get().pass("Login test completed.");
     }
 }
