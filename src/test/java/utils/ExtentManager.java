38369a5f83e4d1584c7dc36c9ec4a8cbc3cfe1d6
Nirusha1 <bhintunasayami123@gmail.com>
2025-06-20 12:17:46 -0400

    Update for Extent report to include theme, timeline, author and test passed designs

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -10,7 +10,18 @@ public class ExtentManager {
         if (extent == null) {
             String path = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
             ExtentSparkReporter sparkReporter = new ExtentSparkReporter(path);
+
+            sparkReporter.config().setReportName("Automation Test Report");
+            sparkReporter.config().setDocumentTitle("Automation Test Report");
+            sparkReporter.config().setTheme(com.aventstack.extentreports.reporter.configuration.Theme.DARK); // or .STANDARD
+            sparkReporter.config().setTimelineEnabled(true); // nice timeline view
             extent = new ExtentReports();
+
+            // Add custom system info:
+            extent.setSystemInfo("Project", "SelAE");
+            extent.setSystemInfo("Tester", "Nirusha Manandhar");
+            extent.setSystemInfo("Browser", "Chrome");
+            extent.setSystemInfo("OS", System.getProperty("os.name"));
             extent.attachReporter(sparkReporter);
         }
         return extent;
